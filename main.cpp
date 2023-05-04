bd8f46afce9f2ddd2119e1e71337541084caae40
adiya <47381588+Adiya21j@users.noreply.github.com>
2023-05-04 15:57:03 +0800

    example.cpp

--- main.cpp
+++ main.cpp
@@ -9,50 +9,24 @@ void setup() {
   display.setup();
   display.cls();
 
-  // put your main code here, to run repeatedly:
+  //any type of same arguments canbe pass
+  display.printLines(200,350,5);
+  delay(5000);
 
-  // display.printLines(200,350,5);
-  // delay(5000);
-
-  // display.print(50005,25,5);
-  // delay(5000);
-
-  // display.print(999999,6000,8888);
-  // delay(5000);
+  //pass any type arguments to each display row and all text align is right.
+  display.printFirstLine(67000);
+  display.printSecondLine("Press");
+  display.printThirdLine(5.6);
+  delay(5000);
   
-  // display.print(45, 21, 9);
-  // delay(3000);
-  // display.print("MARCH", "21", "9");
-  // delay(3000);
-  // display.print(a, b, c);
-  // delay(3000);
-
-  // display.printFirstLine(67000);
-  // display.printSecondLine("Press");
-  // display.printThirdLine(5.6);
-  // delay(5000);
-
-  // display.printFirstLine(500);
-  // display.printSecondLine("Press");
-  // display.printThirdLine(25.6);
-  // delay(5000);
-  // display.printFirstRow("67000");
-  // display.printSecondRow("HELLO");
-  // display.printThirdRow("5.6");
-  // delay(5000);
+  //pass char string arguments to each display row and all text align is left.
+  display.printFirstRow("HI");
+  display.printSecondRow("PAY");
+  display.printThirdRow("EVM");
+  delay(5000);
 }
 
 
 void loop() {
-  display.printFirstLine(52.3);
-  display.printSecondLine("Press");
-  display.printThirdLine(500);
-  delay(5000);
-  display.printFirstRow("67000");
-  display.printSecondRow("HELLO");
-  display.printThirdRow("53.6");
-  delay(5000);
-  uint32_t a=555555, b=444444, c=3333;
-  display.printLines(a,b,c);
-  delay(5000);
+  ;
 }
